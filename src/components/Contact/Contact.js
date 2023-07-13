ecbf08dd7066fcb4b2cbc6cb44b6433085cb0fc8
Jose Mendez <iscjosemendez@gmail.com>
2023-07-13 00:03:01 -0600

    contact

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -6,9 +6,14 @@ const Contact = () => {
   return (
     <ContactStyles className="section">
       <form name="contact" netlify-honeypot="bot-field" data-netlify="true">
-        <input placeholder="Nombre..." type="text" name="name" />
-        <input placeholder="Email..." type="email" name="email" />
-        <textarea placeholder="Mensaje..." name="message" rows="5"></textarea>
+        <input id="name" placeholder="Nombre..." type="text" name="name" />
+        <input id="email" placeholder="Email..." type="email" name="email" />
+        <textarea
+          id="message"
+          placeholder="Mensaje..."
+          name="message"
+          rows="5"
+        ></textarea>
         <Button text="Enviar mensaje" />
       </form>
     </ContactStyles>
