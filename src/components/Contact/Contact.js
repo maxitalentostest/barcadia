ba87cdebcc5e67b5b6e09462f2e06bd9c4adfb05
Jose Mendez <iscjosemendez@gmail.com>
2023-07-12 23:40:08 -0600

    data netlify true

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -5,7 +5,7 @@ import { ContactStyles } from "./ContactStyles"
 const Contact = () => {
   return (
     <ContactStyles className="section">
-      <form name="contact" netlify>
+      <form name="contact" data-netlify="true">
         <input placeholder="Nombre..." type="text" name="name" />
         <input placeholder="Email..." type="email" name="email" />
         <textarea placeholder="Mensaje..." name="message" rows="5"></textarea>
